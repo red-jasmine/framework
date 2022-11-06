ccc947800ee95a80d6d6372a3aa5a5d31a834cb8
liushoukun <liushoukun66@gmail.com>
2022-11-06 12:15:59 +0800

    init

---
+++ src/Support.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace RedJasmine\Support;
+
+class Support
+{
+    // Build wonderful things
+}
---
+++ src/SupportServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace RedJasmine\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+class SupportServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'red-jasmine');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'red-jasmine');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/support.php', 'support');
+
+        // Register the service the package provides.
+        $this->app->singleton('support', function ($app) {
+            return new Support;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['support'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/support.php' => config_path('support.php'),
+        ], 'support.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/red-jasmine'),
+        ], 'support.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/red-jasmine'),
+        ], 'support.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/red-jasmine'),
+        ], 'support.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
