b5a0cf6abbc39a5a40e99cab8db59a770b0cc548
liushoukun <liushoukun66@gmail.com>
2023-03-13 22:53:35 +0800

    init

---
+++ src/Address.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace RedJasmine\Address;
+
+class Address
+{
+    // Build wonderful things
+}
---
+++ src/Facades/Address.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RedJasmine\Address\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Address extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'address';
+    }
+}
