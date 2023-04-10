460432b2189d50787c73a3804d0ae722e29de5f3
liushoukun <liushoukun66@gmail.com>
2023-04-10 14:19:01 +0800

    添加用户生成器

---
+++ src/Helpers/UserObjectBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RedJasmine\Support\Helpers;
+
+use RedJasmine\Support\Contracts\UserInterface;
+
+class UserObjectBuilder implements UserInterface
+{
+    public function __construct(public array $data)
+    {
+    }
+
+    /**
+     * 用户类型
+     * @return string|int
+     */
+    public function getUserType() : string|int
+    {
+        return $this->data['type'];
+    }
+
+    /**
+     * 获取用户ID
+     * @return int|string
+     */
+    public function getUID() : string|int
+    {
+        return $this->data['uid'];
+    }
+
+    /**
+     * 获取昵称
+     * @return string|null
+     */
+    public function getNickname() : ?string
+    {
+        return (string)($this->data['nickname'] ?? null);
+    }
+
+    /**
+     * 获取头像
+     * @return string|null
+     */
+    public function getAvatar() : ?string
+    {
+        return (string)($this->data['avatar'] ?? null);
+    }
+
+
+}
