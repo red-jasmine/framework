e3a457c87c9d54e988c58fa711c2271398554e6f
liushoukun <liushoukun66@gmail.com>
2023-08-14 23:48:10 +0800

    feat(枚举辅助): 添加options方法

--- src/Helpers/Enums/EnumsHelper.php
+++ src/Helpers/Enums/EnumsHelper.php
@@ -8,12 +8,26 @@ trait EnumsHelper
 
     public function name() : string
     {
-        return self::names()[$this->value]??$this->value;
+        return self::names()[$this->value] ?? $this->value;
     }
 
     public function color()
     {
-        return self::colors()[$this->value]??$this->value;
+        return self::colors()[$this->value] ?? $this->value;
+    }
+
+
+    public static function options() : array
+    {
+        $names = self::names();
+
+        return array_map(function ($key, $value) {
+            return [
+                'value' => $key,
+                'label' => $value
+            ];
+        }, array_keys($names), $names);
+
     }
 
 }
