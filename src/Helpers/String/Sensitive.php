556460111bec8bdc177c1e31d56c907ff22f2e95
liushoukun <liushoukun66@gmail.com>
2023-12-19 17:41:06 +0800

    refactor: 字符串脱敏

---
+++ src/Helpers/String/Sensitive.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RedJasmine\Support\Helpers\String;
+
+use Illuminate\Support\Str;
+
+class Sensitive
+{
+
+
+    public string $mask = '*';
+
+    public function string(string $string = null, $showLastCharacters = 2) : ?string
+    {
+        if (blank($string)) {
+            return null;
+        }
+        return str_repeat($this->mask, mb_strlen($string, 'UTF-8') - $showLastCharacters) .
+               mb_substr($string, -$showLastCharacters, null, 'UTF-8');
+
+    }
+
+}
