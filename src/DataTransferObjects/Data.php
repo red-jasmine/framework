7350e56af4a707b1bfccb87c532636d462d685df
liushoukun <liushoukun66@gmail.com>
2024-02-22 00:10:50 +0800

    feat: 订单发货修改

---
+++ src/DataTransferObjects/Data.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RedJasmine\Support\DataTransferObjects;
+
+
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\MapOutputName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+
+#[MapInputName(SnakeCaseMapper::class)]
+#[MapOutputName(SnakeCaseMapper::class)]
+class Data extends \Spatie\LaravelData\Data
+{
+
+}
