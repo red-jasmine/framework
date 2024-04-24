134d536177ac40c76134674ee48203871450f181
liushoukun <liushoukun66@gmail.com>
2024-04-24 20:58:27 +0800

    refactor: 服务组件

---
+++ src/Foundation/HasServiceContext.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RedJasmine\Support\Foundation;
+
+use Closure;
+use RedJasmine\Support\Contracts\UserInterface;
+
+trait HasServiceContext
+{
+
+    /**
+     * 操作人
+     * @var UserInterface|Closure|null
+     */
+    protected UserInterface|Closure|null $operator = null;
+
+    public function getOperator() : UserInterface|Closure|null
+    {
+        $operator = $this->operator;
+        if (is_callable($operator)) {
+            return $operator();
+        }
+        return $operator;
+    }
+
+    public function setOperator(UserInterface|Closure|null $operator) : static
+    {
+        $this->operator = $operator;
+        return $this;
+    }
+
+
+}
