9a0665282e47bba13a01cc73f171ca98b9acbefd
liushoukun <liushoukun66@gmail.com>
2024-08-10 16:40:34 +0800

    docs: 字段修改

--- database/migrations/2023_12_11_225521_create_product_stock_logs_table.php
+++ database/migrations/2023_12_11_225521_create_product_stock_logs_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use RedJasmine\Product\Domain\Stock\Models\Enums\ProductStockChangeTypeEnum;
+use RedJasmine\Product\Domain\Stock\Models\Enums\ProductStockTypeEnum;
 
 return new class extends Migration {
     public function up() : void
@@ -12,8 +14,8 @@ return new class extends Migration {
             $table->morphs('owner');
             $table->unsignedBigInteger('product_id')->comment('商品ID');
             $table->unsignedBigInteger('sku_id')->comment('SKU ID');
-            $table->string('type', 32)->comment('操作类型');
-            $table->string('change_type', 32)->comment('更变类型');
+            $table->string('type', 32)->comment(ProductStockTypeEnum::comments('操作类型'));
+            $table->string('change_type', 32)->comment(ProductStockChangeTypeEnum::comments('变更类型'));
             $table->string('change_detail')->nullable()->comment('变更明细');
             $table->bigInteger('stock')->comment('库存');
             $table->bigInteger('lock_stock')->default(0)->comment('锁定库存');
