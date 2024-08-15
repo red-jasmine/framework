eb2b4c3c0ebe23d336fcfb84e4ec15fde3457eff
liushoukun <liushoukun66@gmail.com>
2024-08-15 18:16:34 +0800

    test: 价格获取

---
+++ src/Domain/Price/ProductPriceDomainService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RedJasmine\Product\Domain\Price;
+
+use RedJasmine\Ecommerce\Domain\Models\ValueObjects\Amount;
+use RedJasmine\Product\Domain\Product\Models\Product;
+use RedJasmine\Support\Foundation\Service\Service;
+
+class ProductPriceDomainService extends Service
+{
+    // 影响价格因素
+    // 数量、卖家
+
+
+    public function getPrice(Product $product, int $skuID) : Amount
+    {
+        $sku = $product->skus->where('id', $skuID)->firstOrFail();
+        // 获取价格
+
+        return $sku->price;
+    }
+
+}
