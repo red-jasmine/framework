1ffe4449622b6810364c62a9465a3fc21515f171
liushoukun <liushoukun66@gmail.com>
2024-08-19 23:17:36 +0800

    test: 订单修改

--- src/Domain/Models/ValueObjects/Amount.php
+++ src/Domain/Models/ValueObjects/Amount.php
@@ -3,16 +3,52 @@
 namespace RedJasmine\Ecommerce\Domain\Models\ValueObjects;
 
 use http\Exception\InvalidArgumentException;
-use Illuminate\Contracts\Support\Jsonable;
 use RedJasmine\Support\Domain\Models\ValueObjects\ValueObject;
 use RedJasmine\Support\Domain\ValueObjects\Exception;
 
 
-class Amount extends ValueObject implements Jsonable
+class Amount extends ValueObject
 {
 
 
     protected int $scale = 2;
+    /**
+     * @var string
+     */
+    private string $value;
+
+    /**
+     * @param  float|int|string|Amount|null  $value
+     *
+     */
+    public function __construct(float|int|string|null|self $value = 0)
+    {
+        if ($value instanceof static) {
+            $value = $value->value();
+        }
+        $this->value = bcadd($value, 0, $this->scale);
+    }
+
+    public function value() : string
+    {
+        return bcadd($this->value, 0, $this->scale);
+    }
+
+    public static function make() : Amount
+    {
+        $args = func_get_args();
+        return new static(...$args);
+    }
+
+    /**
+     * @return void
+     */
+    public function validateMin() : void
+    {
+        if (bccomp($this->value, 0, $this->scale) < 0) {
+            throw new InvalidArgumentException('金额不能小于0');
+        }
+    }
 
     public function getScale() : int
     {
@@ -25,62 +61,20 @@ class Amount extends ValueObject implements Jsonable
         return $this;
     }
 
-
-    public static function make() : Amount
-    {
-        $args = func_get_args();
-        return new static(...$args);
-    }
-
-    /**
-     * @var string
-     */
-    private string $value;
-
-    /**
-     * @param  float|int|string|null  $value
-     *
-     * @throws Exception
-     */
-    public function __construct(float|int|string|null $value = 0)
-    {
-
-        $this->value = bcadd($value, 0, $this->scale);
-
-        $this->validateMin();
-    }
-
-
-    /**
-     * @return void
-     */
-    public function validateMin() : void
-    {
-        if (bccomp($this->value, 0, $this->scale) < 0) {
-            throw new InvalidArgumentException('金额不能小于0');
-        }
-    }
-
-    public function value() : string
-    {
-        return bcadd($this->value, 0, $this->scale);
-    }
-
-    protected function setValue($value) : void
-    {
-        $this->value = bcadd($value, 0, $this->scale);
-    }
-
     public function __toString() : string
     {
         return $this->value();
     }
 
-
     public function sub($value) : self
     {
         $this->setValue(bcadd($this->value, (string) $value, $this->scale));
         return $this;
+    }
+
+    protected function setValue($value) : void
+    {
+        $this->value = bcadd($value, 0, $this->scale);
     }
 
     public function add($value) : self
@@ -94,7 +88,6 @@ class Amount extends ValueObject implements Jsonable
         $this->setValue(bcmul($this->value, (string) $value, $this->scale));
         return $this;
     }
-
 
 
 }
