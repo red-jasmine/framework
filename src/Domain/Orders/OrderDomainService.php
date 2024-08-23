5b8556621848da09aeb4d10fb30ffd464d73c6ad
liushoukun <liushoukun66@gmail.com>
2024-08-23 23:11:01 +0800

    feat(下单流程): 流程优化

--- src/Domain/Orders/OrderDomainService.php
+++ src/Domain/Orders/OrderDomainService.php
@@ -102,42 +102,9 @@ class OrderDomainService extends Service
      */
     public function split(OrderData $orderData) : OrdersData
     {
-        // 拆分订单
-        $orders       = new OrdersData();
-        $orderCollect = collect();
-
-        $productGroup = $orderData->products
-            ->each(function ($product) {
-                // 订单拆分 默认区分卖家来拆分订单
-                $product->setSplitKey(ShoppingOrderSplitProductHook::hook($product,
-                    fn() => $this->getProductSplitKey($product))
-                );
-            })
-            ->groupBy(function ($product) {
-                return $product->getSplitKey();
-            })->all();
-
-        foreach ($productGroup as $splitKey => $products) {
-            $order  = clone $orderData;
-            $seller = $products[0]->getProduct()->owner;
-            $order->setSeller($seller);
-            $order->products = collect($products);
-            $orderCollect->push($order);
-        }
-        $orders->setOrders($orderCollect);
-
-        return $orders;
+        return (new OrderSplitService())->split($orderData);
     }
 
-    protected function getProductSplitKey(ProductData $productData) : string
-    {
-        $implode = [
-            $productData->getProduct()->owner->getType(),
-            $productData->getProduct()->owner->getID(),
-        ];
-        // 判断是否存特殊的逻辑
-        return implode('|', $implode);
-    }
 
     /**
      * 订单金额计算
