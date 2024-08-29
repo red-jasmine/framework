fe0749f74edfa0ded0625ce2167d19de149ec152
liushoukun <liushoukun66@gmail.com>
2024-08-29 23:36:55 +0800

    refactor(shopping): 简化OrderBuyCommandHandler并使用基类事务管理
    重构OrderBuyCommandHandler，移除直接的DB事务调用，现在使用基类提供的事务管理方法。这减少了代码冗余，并利用了已经存在的父类功能，提高了代码的可维护性和一致性。
    
    更改包括：
    - 构造函数中调用parent::__construct()初始化基类。
    - 通过父类方法开始和提交事务，替代直接的DB::beginTransaction()和DB::commit()调用。
    - 异常处理中使用基类的回滚方法，替代直接的DB::rollBack()。
    
    这避免了在子类中直接操作数据库事务，提高了代码的整洁度和逻辑一致性。

--- src/Application/Services/CommandHandlers/OrderBuyCommandHandler.php
+++ src/Application/Services/CommandHandlers/OrderBuyCommandHandler.php
@@ -9,15 +9,21 @@ use RedJasmine\Shopping\Application\UserCases\Commands\ProductBuyCommand;
 use RedJasmine\Shopping\Domain\Orders\OrderDomainService;
 use RedJasmine\Support\Application\CommandHandler;
 use RedJasmine\Support\Exceptions\AbstractException;
+use Throwable;
 
+// 定义一个处理购买命令的类，继承自CommandHandler基类
 class OrderBuyCommandHandler extends CommandHandler
 {
+    // 构造函数
+    // 初始化必要的服务并开始数据库事务
     public function __construct(
         protected OrderDomainService $orderDomainService,
-
     ) {
-
+        // 调用父类的构造方法进行初始化
         parent::__construct();
+
+        // 开始数据库事务，确保数据的一致性
+        $this->databaseTransaction();
     }
 
 
@@ -26,31 +32,24 @@ class OrderBuyCommandHandler extends CommandHandler
      *
      * @return \Illuminate\Support\Collection
      * @throws AbstractException
+     * @throws Throwable
      */
     public function handle(ProductBuyCommand $command)
     {
-
+        $this->beginDatabaseTransaction();
         try {
-            DB::beginTransaction();
-
-            // 下单
             $orders = $this->orderDomainService->buy($command);
 
-
-            DB::commit();
+            $this->commitDatabaseTransaction();
         } catch (AbstractException $exception) {
-            DB::rollBack();
+            $this->rollBackDatabaseTransaction();
             Log::info('下单失败:'.$exception->getMessage(), $command->toArray());
-            throw  $exception;
+            throw $exception;
         } catch (Throwable $throwable) {
-            DB::rollBack();
-            throw  $throwable;
+            $this->rollBackDatabaseTransaction();
+            throw $throwable;
         }
-
-
         return $orders;
-
-
     }
-
 }
+
