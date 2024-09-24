fbc72794cdcfc0e66a0f77485aa49b2caa14aa56
liushoukun <liushoukun66@gmail.com>
2024-09-25 00:27:48 +0800

    枚举帮助工具中添加颜色分配方法
    在枚举帮助特质中添加了新的静态方法`colors()`，该方法根据预定义的基础颜色数组分配颜色给枚举值。此方法使得枚举值有了对应的颜色表示，增强了枚举在视觉展示上的一致性和易用性。

--- src/Helpers/Enums/EnumsHelper.php
+++ src/Helpers/Enums/EnumsHelper.php
@@ -3,8 +3,11 @@
 namespace RedJasmine\Support\Helpers\Enums;
 
 
+use phpDocumentor\Reflection\Types\Static_;
+
 trait EnumsHelper
 {
+
 
     public function name() : string
     {
@@ -39,9 +42,37 @@ trait EnumsHelper
     public static function comments(string $title = '') : string
     {
         $enums = array_map(function ($key, $value) {
-            return $key . '(' . $value.')';
+            return $key.'('.$value.')';
         }, array_keys(static::labels()), static::labels());
-        return $title . ': ' . implode(',', $enums);
+        return $title.': '.implode(',', $enums);
+
+    }
+
+    // 定义一个基础颜色数组函数
+
+    public static function baseColors() : array
+    {
+        return [
+            'primary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'dark',
+        ];
+    }
+
+    public static function colors() : array
+    {
+        // 根据所有枚举值按顺序分配颜色 返回一个枚举值对于 颜色的数组
+        $baseColors = static::baseColors();
+        $count      = count(self::values());
+        $colors     = [];
+        foreach (self::values() as $index => $value) {
+            $colors[$value] = $baseColors[$index % $count];
+        }
+
+        return $colors;
 
     }
 
