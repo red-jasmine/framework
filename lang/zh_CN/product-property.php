9836b969f695d6260c17060432136a1fd3659a6b
liushoukun <liushoukun66@gmail.com>
2024-10-01 23:04:24 +0800

    优化产品属性相关资源的表单和表格显示
    - 更新了ProductPropertyGroupResource和ProductPropertyResource中的表单和表格标签
    -使其与相应的语言文件中的字段名称保持一致
    - 在product-property.php语言文件中添加了缺失的字段翻译

--- lang/zh_CN/product-property.php
+++ lang/zh_CN/product-property.php
@@ -6,7 +6,9 @@ return [
     ],
     'fields'  => [
         'pid'          => '属性ID',
+        'type'         => '类型',
         'name'         => '名称',
+        'description'  => '描述',
         'sort'         => '排序',
         'status'       => '状态',
         'group_id'     => '分组',
@@ -15,6 +17,7 @@ return [
         'creator_id'   => '创建者UID',
         'updater_type' => '更新者类型',
         'updater_id'   => '更新者UID',
+        'is_allow_multiple'   => '是否允许多值',
         'group'        => [
             'name' => '分组'
         ],
