f6dfd416079f5bd05bbd44584d3896ad844d47e1
liushoukun <liushoukun66@gmail.com>
2024-10-27 15:46:36 +0800

    feat(order): 添加订单退款功能并优化事务处理
    - 在 RefundCreateCommandHandler 中添加数据库事务处理，确保数据一致性
    - 更新 Order 和 OrderRefundService 类，支持创建退款并返回退款对象
    - 添加订单退款相关单元测试，验证退款功能的正确性

---
+++ tests/Feature/Order/Application/OrderStandardFlowDummyRefundTest.php
@@ -0,0 +1,142 @@
+'<?php
+
+
+use RedJasmine\Ecommerce\Domain\Models\Enums\RefundTypeEnum;
+use RedJasmine\Ecommerce\Domain\Models\Enums\ShippingTypeEnum;
+use RedJasmine\Order\Application\Services\OrderCommandService;
+use RedJasmine\Order\Application\Services\RefundCommandService;
+use RedJasmine\Order\Application\UserCases\Commands\OrderCreateCommand;
+use RedJasmine\Order\Application\UserCases\Commands\OrderPaidCommand;
+use RedJasmine\Order\Application\UserCases\Commands\OrderPayingCommand;
+use RedJasmine\Order\Application\UserCases\Commands\Refund\RefundCreateCommand;
+use RedJasmine\Order\Domain\Models\Enums\OrderTypeEnum;
+use RedJasmine\Order\Domain\Models\Enums\PaymentStatusEnum;
+use RedJasmine\Order\Domain\Models\Order;
+use RedJasmine\Order\Domain\Models\OrderPayment;
+use RedJasmine\Order\Domain\Repositories\OrderReadRepositoryInterface;
+use RedJasmine\Order\Domain\Repositories\OrderRepositoryInterface;
+use RedJasmine\Tests\Feature\Order\Fixtures\OrderFake;
+
+
+beforeEach(function () {
+
+    $this->orderReadRepository  = app(OrderReadRepositoryInterface::class);
+    $this->orderRepository      = app(OrderRepositoryInterface::class);
+    $this->orderCommandService  = app(OrderCommandService::class);
+    $this->refundCommandService = app(RefundCommandService::class);
+
+    $orderFake               = new OrderFake();
+    $orderFake->orderType    = OrderTypeEnum::STANDARD;
+    $orderFake->shippingType = ShippingTypeEnum::DUMMY;
+    $this->orderFake         = $orderFake;
+    //
+});
+
+test('can create a new order', function () {
+
+
+    $command = OrderCreateCommand::from($this->orderFake->order());
+    $result  = $this->orderCommandService->create($command);
+
+    $this->assertInstanceOf(Order::class, $result, '创建订单失败');
+    $this->order = $result;
+
+    return $result;
+});
+
+
+test('cna paying a order', function (Order $order) {
+
+
+    //Event::fake();
+
+    $command = OrderPayingCommand::from(
+        [
+            'id'     => $order->id,
+            'amount' => $order->payable_amount
+
+        ]
+
+    );
+
+
+    $result = $this->orderCommandService->paying($command);
+
+    //Event::assertDispatched(OrderPayingEvent::class, null);
+
+    $this->assertInstanceOf(OrderPayment::class, $result, '创建支付记录失败');
+
+
+    return $result;
+
+})->depends('can create a new order');
+
+
+test('can paid a order', function (Order $order, OrderPayment $orderPayment) {
+
+
+    $command = new  OrderPaidCommand;
+
+    $command->id               = $order->id;
+    $command->orderPaymentId   = $orderPayment->id;
+    $command->amount           = $orderPayment->payment_amount;
+    $command->paymentType      = 'online';
+    $command->paymentId        = fake()->numberBetween(1000000, 999999999);
+    $command->paymentChannel   = 'alipay';
+    $command->paymentChannelNo = fake()->numerify('channel-no-########');
+    $command->paymentTime      = date('Y-m-d H:i:s');
+
+
+    $result = $this->orderCommandService->paid($command);
+
+
+    $this->assertTrue($result);
+
+    $order = $this->orderRepository->find($order->id);
+
+    $this->assertEquals(PaymentStatusEnum::PAID->value, $order->payment_status->value);
+    $this->assertEquals($order->payable_amount->value(), $order->payment_amount->value());
+    return $result;
+
+})->depends('can create a new order', 'cna paying a order');
+
+
+// 退款
+
+
+test('can refund a order', function (Order $order, OrderPayment $orderPayment) {
+
+    $commands    = [];
+    $command     = new RefundCreateCommand;
+    $command->id = $order->id;
+
+    $refunds = [];
+    foreach ($order->products as $product) {
+        $command                 = new RefundCreateCommand;
+        $command->id             = $order->id;
+        $command->orderProductId = $product->id;
+        $command->refundType     = RefundTypeEnum::REFUND;
+        $command->refundAmount   = $product->payment_amount;
+        $command->reason         = '不想要了';
+        $command->description    = fake()->sentence;
+        $command->outerRefundId  = fake()->numerify('######');
+        $command->images         = [ fake()->imageUrl, fake()->imageUrl, fake()->imageUrl, ];
+
+        $commands [] = $command;
+
+
+        $refunds[] = $this->refundCommandService->create($command);
+    }
+    dd($refunds);
+
+
+})->depends('can create a new order', 'cna paying a order', 'can paid a order');
+
+
+
+
+
+
+
+
+
