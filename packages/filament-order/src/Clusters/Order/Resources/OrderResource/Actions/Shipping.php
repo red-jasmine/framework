c8dab3011e900547b5feb7c2e68df3f21a00d4fa
liushoukun <liushoukun66@gmail.com>
2024-11-05 18:36:07 +0800

    refactor(framework): 重构订单发货形式
    - 引入 ShippingTypeEnum 枚举类
    -根据不同的发货类型动态显示表单
    - 新增 cdkForm、dummyForm 和 expressForm 方法分别处理不同发货类型的表单
    - 优化了表单组件的使用，提高了代码的可读性和可维护性

--- packages/filament-order/src/Clusters/Order/Resources/OrderResource/Actions/Shipping.php
+++ packages/filament-order/src/Clusters/Order/Resources/OrderResource/Actions/Shipping.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace RedJasmine\FilamentOrder\Clusters\Order\Resources\OrderResource\Actions;
+
 use Filament\Forms;
+use RedJasmine\Ecommerce\Domain\Models\Enums\ShippingTypeEnum;
 
 trait Shipping
 {
@@ -14,23 +16,78 @@ trait Shipping
 
         $this->visible(fn($record) => $record->isAllowShipping());
 
+
         $this->form(function ($record) {
-            return [
+            return match ($record->shipping_type) {
+                ShippingTypeEnum::DUMMY => $this->dummyForm($record),
+                ShippingTypeEnum::EXPRESS => $this->expressForm($record),
+                ShippingTypeEnum::CDK => $this->cdkForm($record),
+                ShippingTypeEnum::DELIVERY => $this->dummyForm($record),
+                ShippingTypeEnum::NONE => $this->dummyForm($record),
 
-                Forms\Components\CheckboxList::make('order_products')
-                                             ->label(__('red-jasmine-order::order.fields.products'))
-                                             ->options($record->products->pluck('title', 'id')->toArray()),
+            };
 
-                Forms\Components\ToggleButtons::make('is_finished')
-                                              ->label(__('red-jasmine-order::commands.shipping.is_finished'))
-                                              ->default(true)
-                                              ->grouped()
-                                              ->boolean()
 
-            ];
         });
 
 
-
     }
+
+
+    protected  function cdkForm($record) : array
+    {
+        return [
+
+            Forms\Components\Radio::make('order_product_id')
+                                         ->label(__('red-jasmine-order::order.fields.products'))
+                                         ->options($record->products->pluck('title', 'id')->toArray()),
+
+
+
+        ];
+    }
+
+    protected function dummyForm($record) : array
+    {
+        return [
+
+            Forms\Components\CheckboxList::make('order_products')
+                                         ->label(__('red-jasmine-order::order.fields.products'))
+                                         ->options($record->products->pluck('title', 'id')->toArray()),
+
+            Forms\Components\ToggleButtons::make('is_finished')
+                                          ->label(__('red-jasmine-order::commands.shipping.is_finished'))
+                                          ->default(true)
+                                          ->grouped()
+                                          ->boolean()
+
+        ];
+    }
+
+    protected function expressForm($record) : array
+    {
+        return  [
+
+            Forms\Components\ToggleButtons::make('is_split')
+                                          ->label(__('red-jasmine-order::commands.shipping.is_split'))
+                                          ->default(false)
+                                          ->grouped()
+                                          ->live()
+                                          ->boolean(),
+            Forms\Components\CheckboxList::make('order_products')
+                                         ->label(__('red-jasmine-order::commands.shipping.products'))
+                                         ->visible(fn(Forms\Get $get) => $get('is_split'))
+                                         ->options($record->products->pluck('title', 'id')->toArray()),
+
+            Forms\Components\TextInput::make('express_company_code')
+                                      ->label(__('red-jasmine-order::commands.shipping.express_company_code'))
+                                      ->required(),
+            Forms\Components\TextInput::make('express_no')
+                                      ->label(__('red-jasmine-order::commands.shipping.express_no'))
+                                      ->required(),
+
+        ];
+    }
+
+
 }
