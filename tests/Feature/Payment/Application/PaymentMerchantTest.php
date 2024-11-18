e03f2c97c9e03cdaec233dbca90ee1f729424cf7
liushoukun <liushoukun66@gmail.com>
2024-11-18 17:23:16 +0800

    feat(payment): 创建商户相关的命令和服务
    - 新增商户创建、更新和设置状态的命令类
    - 实现商户命令的处理逻辑- 添加商户相关的服务提供者- 更新数据库迁移文件，修正表名和字段
    - 新增商户模型和转换器
    - 实现商户读取和写入的仓库接口和具体实现

---
+++ tests/Feature/Payment/Application/PaymentMerchantTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use RedJasmine\Payment\Application\Commands\Merchant\MerchantCreateCommand;
+use RedJasmine\Payment\Application\Commands\Merchant\MerchantSetStatusCommand;
+use RedJasmine\Payment\Application\Commands\Merchant\MerchantUpdateCommand;
+use RedJasmine\Payment\Application\Services\MerchantCommandService;
+use RedJasmine\Payment\Domain\Models\Enums\MerchantStatusEnum;
+use RedJasmine\Payment\Domain\Models\PaymentMerchant;
+use RedJasmine\Payment\Domain\Repositories\MerchantRepositoryInterface;
+use RedJasmine\Support\Data\UserData;
+
+beforeEach(function () {
+
+    $this->paymentMerchantRepository     = app(MerchantRepositoryInterface::class);
+    $this->paymentMerchantCommandService = app(MerchantCommandService::class);
+
+
+    //
+});
+
+
+test('can create merchant', function () {
+
+    $command = new MerchantCreateCommand();
+
+
+    $command->owner = UserData::from([ 'type' => 'user', 'id' => 1 ]);
+
+    $command->name      = 'XXX有限公司';
+    $command->shortName = '测试';
+
+
+    $merchant = $this->paymentMerchantCommandService->create($command);
+
+
+    $this->assertEquals($command->name, $merchant->name, '商户名称');
+
+    return $merchant;
+});
+
+test('can set status', function (PaymentMerchant $merchant) {
+    $command         = new MerchantSetStatusCommand();
+    $command->id     = $merchant->id;
+    $command->status = MerchantStatusEnum::DISABLED;
+    $this->paymentMerchantCommandService->setStatus($command);
+
+    $merchant = $this->paymentMerchantRepository->find($command->id);
+
+    $this->assertEquals($command->status->value, $merchant->status->value, '商户状态');
+    return $merchant;
+})->depends('can create merchant');
+
+
+test('can update merchant', function (PaymentMerchant $merchant) {
+
+    $command     = new MerchantUpdateCommand();
+    $command->id = $merchant->id;
+
+    $command->owner = UserData::from([ 'type' => 'user', 'id' => 1 ]);
+
+    $command->name      = 'XXXX有限公司';
+    $command->shortName = '测试';
+
+
+    $merchant = $this->paymentMerchantCommandService->update($command);
+
+    $this->assertEquals($command->name, $merchant->name, '商户名称');
+
+    return $merchant;
+})->depends('can create merchant');
+
