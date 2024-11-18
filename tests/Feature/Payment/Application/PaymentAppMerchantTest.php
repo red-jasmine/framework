1f745f462ffc64b2833c216fb2d070ac3cfec1ae
liushoukun <liushoukun66@gmail.com>
2024-11-18 18:13:13 +0800

    feat(payment): 新增商户应用相关功能
    - 新增商户应用创建和更新功能
    - 实现商户应用状态枚举
    - 添加商户应用相关数据库迁移
    - 创建商户应用命令和服务类
    - 编写商户应用单元测试

---
+++ tests/Feature/Payment/Application/PaymentAppMerchantTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use RedJasmine\Payment\Application\Commands\Merchant\MerchantCreateCommand;
+use RedJasmine\Payment\Application\Commands\MerchantApp\MerchantAppCreateCommand;
+use RedJasmine\Payment\Application\Commands\MerchantApp\MerchantAppUpdateCommand;
+use RedJasmine\Payment\Application\Services\MerchantAppCommandService;
+use RedJasmine\Payment\Application\Services\MerchantCommandService;
+use RedJasmine\Payment\Domain\Models\Enums\MerchantAppStatusEnum;
+use RedJasmine\Payment\Domain\Models\PaymentMerchant;
+use RedJasmine\Payment\Domain\Models\PaymentMerchantApp;
+use RedJasmine\Payment\Domain\Repositories\MerchantAppRepositoryInterface;
+use RedJasmine\Payment\Domain\Repositories\MerchantRepositoryInterface;
+use RedJasmine\Support\Data\UserData;
+
+beforeEach(function () {
+
+    $this->paymentMerchantRepository     = app(MerchantRepositoryInterface::class);
+    $this->paymentMerchantCommandService = app(MerchantCommandService::class);
+
+    $this->paymentMerchantAppRepository     = app(MerchantAppRepositoryInterface::class);
+    $this->paymentMerchantAppCommandService = app(MerchantAppCommandService::class);
+    //
+});
+
+
+test('can create merchant', function () {
+
+    $command = new MerchantCreateCommand();
+
+
+    $command->owner = UserData::from([ 'type' => 'user', 'id' => 1 ]);
+
+    $command->name      = 'XXX有限公司';
+    $command->shortName = '测试';
+
+
+    $merchant = $this->paymentMerchantCommandService->create($command);
+
+
+    $this->assertEquals($command->name, $merchant->name, '商户名称');
+
+    return $merchant;
+});
+
+test('can create a merchant app', function (PaymentMerchant $merchant) {
+
+
+    $command             = new MerchantAppCreateCommand();
+    $command->name       = fake()->company;
+    $command->merchantId = $merchant->id;
+
+    $model = $this->paymentMerchantAppCommandService->create($command);
+
+    $this->assertEquals($command->name, $model->name, '商户应用名称');
+
+    return $model;
+
+})->depends('can create merchant');
+
+
+test('can update a merchant app', function (PaymentMerchantApp $merchantApp) {
+
+
+    $command             = new MerchantAppUpdateCommand();
+    $command->id         = $merchantApp->id;
+    $command->name       = fake()->company;
+    $command->merchantId = $merchantApp->merchant_id;
+    $command->status     = MerchantAppStatusEnum::DISABLED;
+    $model               = $this->paymentMerchantAppCommandService->update($command);
+
+    $this->assertEquals($command->name, $model->name, '商户应用名称');
+    $this->assertEquals($command->status->value, $model->status->value, ' 商户应用状态');
+
+})->depends('can create a merchant app');
+
+
