5b8f8e4c3ffe37c3cfb2927d3b1013c4abec131c
liushoukun <liushoukun66@gmail.com>
2024-11-19 22:08:35 +0800

    feat(payment): 添加支付渠道应用相关功能
    - 新增支付渠道应用数据模型和迁移文件
    - 添加支付渠道应用创建和更新命令- 实现支付渠道应用的加密存储
    - 编写支付渠道应用的单元测试

---
+++ tests/Feature/Payment/Application/PaymentChannelAppTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use RedJasmine\Payment\Application\Commands\ChannelApp\ChannelAppCreateCommand;
+use RedJasmine\Payment\Application\Commands\ChannelApp\ChannelAppUpdateCommand;
+use RedJasmine\Payment\Application\Services\ChannelAppCommandService;
+use RedJasmine\Payment\Domain\Models\PaymentChannelApp;
+use RedJasmine\Payment\Domain\Repositories\ChannelAppRepositoryInterface;
+use RedJasmine\Support\Data\UserData;
+
+beforeEach(function () {
+
+    $this->commandService = app(ChannelAppCommandService::class);
+    $this->repository     = app(ChannelAppRepositoryInterface::class);
+    //
+
+    $this->owner = UserData::from([ 'type' => 'user', 'id' => 1 ]);
+});
+
+test('create payment channel app', function () {
+
+
+    $command = new ChannelAppCreateCommand();
+
+    $command->owner   = $this->owner;
+    $command->channel = 'alipay';
+
+    $command->channelMerchantId    = fake()->numerify('channel-merchant-id-########');
+    $command->channelAppId         = fake()->numerify('channel-app-id-########');
+    $command->channelAppPublicKey  = fake()->text(3000);//
+    $command->channelPublicKey     = fake()->text(3000);//
+    $command->channelAppPrivateKey = fake()->text(3000);//
+
+    $model = $this->commandService->create($command);
+
+    $model = $this->repository->find($model->id);
+
+    $this->assertEquals($command->channel, $model->channel);
+    $this->assertEquals($command->channelMerchantId, $model->channel_merchant_id);
+    $this->assertEquals($command->channelAppId, $model->channel_app_id);
+    $this->assertEquals($command->channelAppPublicKey, $model->channel_app_public_key);
+    $this->assertEquals($command->channelPublicKey, $model->channel_public_key);
+    $this->assertEquals($command->channelAppPrivateKey, $model->channel_app_private_key);
+
+
+    return $model;
+});
+
+test('update payment channel app', function (PaymentChannelApp $channelApp) {
+
+    $command                       = new ChannelAppUpdateCommand();
+    $command->id                   = $channelApp->id;
+    $command->channel              = 'wechatpay';
+    $command->channelMerchantId    = fake()->numerify('channel-merchant-id-########');
+    $command->channelAppId         = fake()->numerify('channel-app-id-########');
+    $command->channelAppPublicKey  = fake()->text(3000);//
+    $command->channelPublicKey     = fake()->text(3000);//
+    $command->channelAppPrivateKey = fake()->text(3000);//
+
+    $model = $this->commandService->update($command);
+
+    $model = $this->repository->find($model->id);
+
+    $this->assertEquals($command->channel, $model->channel);
+    $this->assertEquals($command->channelMerchantId, $model->channel_merchant_id);
+    $this->assertEquals($command->channelAppId, $model->channel_app_id);
+    $this->assertEquals($command->channelAppPublicKey, $model->channel_app_public_key);
+    $this->assertEquals($command->channelPublicKey, $model->channel_public_key);
+    $this->assertEquals($command->channelAppPrivateKey, $model->channel_app_private_key);
+
+})->depends('create payment channel app');
