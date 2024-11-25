3cc22298d738cdd4fab5e6d34b23403ee7f06fc6
liushoukun <liushoukun66@gmail.com>
2024-11-25 17:56:46 +0800

    feat(payment): 新增预创建支付单功能
    - 新增 TradePreCreateCommand 和 TradePayingCommand 类
    - 实现预创建支付单的逻辑和数据转换
    - 更新数据库迁移，增加支付单预创建状态
    - 重构部分代码以支持新功能

--- tests/Feature/Payment/Application/PaymentTradeTest.php
+++ tests/Feature/Payment/Application/PaymentTradeTest.php
@@ -1,26 +1,87 @@
 <?php
 
 
-use Money\Currencies\ISOCurrencies;
-use Money\Formatter\IntlMoneyFormatter;
-use RedJasmine\Payment\Domain\Data\TradeData;
+use RedJasmine\Payment\Application\Commands\Trade\TradePreCreateCommand;
+use RedJasmine\Payment\Application\Services\TradeCommandService;
+use RedJasmine\Payment\Domain\Data\GoodDetailData;
+use RedJasmine\Payment\Domain\Models\Enums\MerchantAppStatusEnum;
+use RedJasmine\Payment\Domain\Models\Enums\MerchantStatusEnum;
+use RedJasmine\Payment\Domain\Models\Enums\MerchantTypeEnum;
+use RedJasmine\Payment\Domain\Models\Enums\TradeStatusEnum;
+use RedJasmine\Payment\Domain\Models\PaymentMerchant;
+use RedJasmine\Payment\Domain\Models\PaymentMerchantApp;
 
-test('make a trade data', function () {
+beforeEach(function () {
+    /**
+     * @var PaymentMerchant $merchant
+     */
+    $this->merchant = PaymentMerchant::firstOrCreate(
+        [
+            'owner_type' => 'user',
+            'owner_id'   => 1,
+            'status'     => MerchantStatusEnum::ENABLE->value,
+            'name'       => '测试商户',
+            'short_name' => '测试',
+            'type'       => MerchantTypeEnum::GENERAL->value,
+        ], [
+            'name' => '测试商户',
+        ]);
 
 
-    $data = TradeData::from([
-                                'currency' => 'CNY',
-                                'amount'   => 1,
-                            ]);
+    $this->merchantApp = PaymentMerchantApp::firstOrCreate([
+                                                               'merchant_id' => $this->merchant->id,
+                                                               'name'        => '测试应用',
+                                                               'status'      => MerchantAppStatusEnum::ENABLE->value
 
-    $money      = new \Money\Money(102, new \Money\Currency('USD'));
-    $currencies = new ISOCurrencies();
+                                                           ],
+                                                           [
+                                                               'merchant_id' => $this->merchant->id,
+                                                               'name'        => '测试应用',
+                                                           ]
+    );
 
-    $numberFormatter = new NumberFormatter('', NumberFormatter::CURRENCY);
-    $moneyFormatter  = new IntlMoneyFormatter($numberFormatter, $currencies);
 
-    dump( $moneyFormatter->format($money)); // outputs $1.00
-    dump( $moneyFormatter->format($money)); // outputs $1.00
+    $this->commandService = app(TradeCommandService::class);
 
+});
+
+test('pre create a payment trade', function () {
+
+
+    $command                  = new  TradePreCreateCommand();
+    $command->merchantAppId   = $this->merchantApp->id;
+    $command->currency        = 'CNY';
+    $command->amount          = 1;
+    $command->merchantOrderNo = fake()->numerify('trade-##########');
+    $command->subject         = '测试支付';
+    $command->description     = '支付描述';
+    $command->goodDetails     = GoodDetailData::collect([
+                                                            [
+                                                                'goods_name' => fake()->word(),
+                                                                'price'      => fake()->randomFloat(2, 90, 100),
+                                                                'quantity'   => fake()->randomNumber(1, 10),
+                                                                'goods_id'   => fake()->numerify('goods-id-########'),
+                                                                'category'   => fake()->word(),
+                                                            ],
+                                                            [
+                                                                'goods_name' => fake()->word(),
+                                                                'price'      => fake()->randomFloat(2, 90, 100),
+                                                                'quantity'   => fake()->randomNumber(1, 10),
+                                                                'goods_id'   => fake()->numerify('goods-id-########'),
+                                                                'category'   => fake()->word(),
+                                                            ],
+                                                        ]);
+
+
+    $trade = $this->commandService->preCreate($command);
+
+
+    $this->assertEquals($trade->merchant_app_id, $command->merchantAppId, '商户应用id不一致');
+    $this->assertEquals($trade->currency, $command->currency, '货币不一致');
+    $this->assertEquals($trade->amount, $command->amount, '金额不一致');
+    $this->assertEquals($trade->merchant_order_no, $command->merchantOrderNo, '商户订单号不一致');
+    $this->assertEquals($trade->subject, $command->subject, '订单主题不一致');
+    $this->assertEquals($trade->description, $command->description, '订单描述不一致');
+    $this->assertEquals($trade->status->value, TradeStatusEnum::PRE->value, '订单状态不一致');
 
 });
