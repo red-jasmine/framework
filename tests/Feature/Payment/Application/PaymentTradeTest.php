372180ad6e755530fc96d3e58667a5dda36ac4b3
liushoukun <liushoukun66@gmail.com>
2024-12-11 19:33:15 +0800

    feat(payment): 创建预支付单并获取支付方式
    - 在 create_payment_trades_table 中添加 device 和 client 字段
    - 实现 TradePreCreateCommandHandler 和 TradeReadyCommandHandler
    - 更新 TradeCommandService，支持预创建和获取支付方式
    - 修改 Trade 模型，移除 SoftDeletes
    - 更新 PaymentRouteService，使用 Environment 对象
    -调整 TradeTransformer，移除 merchant_app_id 字段
    - 更新单元测试，验证预支付单创建和支付方式获取功能

--- tests/Feature/Payment/Application/PaymentTradeTest.php
+++ tests/Feature/Payment/Application/PaymentTradeTest.php
@@ -2,14 +2,18 @@
 
 
 use RedJasmine\Payment\Application\Commands\Trade\TradePreCreateCommand;
+use RedJasmine\Payment\Application\Commands\Trade\TradeReadyCommand;
 use RedJasmine\Payment\Application\Services\TradeCommandService;
 use RedJasmine\Payment\Domain\Data\GoodDetailData;
 use RedJasmine\Payment\Domain\Models\Enums\MerchantAppStatusEnum;
 use RedJasmine\Payment\Domain\Models\Enums\MerchantStatusEnum;
 use RedJasmine\Payment\Domain\Models\Enums\MerchantTypeEnum;
+use RedJasmine\Payment\Domain\Models\Enums\SceneEnum;
 use RedJasmine\Payment\Domain\Models\Enums\TradeStatusEnum;
 use RedJasmine\Payment\Domain\Models\Merchant;
 use RedJasmine\Payment\Domain\Models\MerchantApp;
+use RedJasmine\Payment\Domain\Models\Trade;
+use RedJasmine\Payment\Domain\Models\ValueObjects\Money;
 
 beforeEach(function () {
     /**
@@ -29,28 +33,28 @@ beforeEach(function () {
 
 
     $this->merchantApp = MerchantApp::firstOrCreate([
-                                                               'merchant_id' => $this->merchant->id,
-                                                               'name'        => '测试应用',
-                                                               'status'      => MerchantAppStatusEnum::ENABLE->value
-                                                           ],
+                                                        'merchant_id' => $this->merchant->id,
+                                                        'name'        => '测试应用',
+                                                        'status'      => MerchantAppStatusEnum::ENABLE->value
+                                                    ],
                                                     [
-                                                               'merchant_id' => $this->merchant->id,
-                                                               'name'        => '测试应用',
-                                                           ]
+                                                        'merchant_id' => $this->merchant->id,
+                                                        'name'        => '测试应用',
+                                                    ]
     );
 
 
-    $this->commandService = app(TradeCommandService::class);
+    $this->tradeCommandService = app(TradeCommandService::class);
 
 });
 
 test('pre create a payment trade', function () {
 
 
-    $command                  = new  TradePreCreateCommand();
-    $command->merchantAppId   = $this->merchantApp->id;
-    $command->currency        = 'CNY';
-    $command->amount          = 1;
+    $command                = new  TradePreCreateCommand();
+    $command->merchantAppId = $this->merchantApp->id;
+
+    $command->amount          = Money::from([ 'amount' => 1, 'currency' => 'CNY' ]);
     $command->merchantOrderNo = fake()->numerify('trade-##########');
     $command->subject         = '测试支付';
     $command->description     = '支付描述';
@@ -72,15 +76,39 @@ test('pre create a payment trade', function () {
                                                         ]);
 
 
-    $trade = $this->commandService->preCreate($command);
+    $trade = $this->tradeCommandService->preCreate($command);
 
 
     $this->assertEquals($trade->merchant_app_id, $command->merchantAppId, '商户应用id不一致');
-    $this->assertEquals($trade->currency, $command->currency, '货币不一致');
-    $this->assertEquals($trade->amount, $command->amount, '金额不一致');
+    $this->assertEquals($trade->amount_currency, $command->amount->currency, '货币不一致');
+    $this->assertEquals($trade->amount_value, $command->amount->amount, '金额不一致');
     $this->assertEquals($trade->merchant_order_no, $command->merchantOrderNo, '商户订单号不一致');
     $this->assertEquals($trade->subject, $command->subject, '订单主题不一致');
     $this->assertEquals($trade->description, $command->description, '订单描述不一致');
     $this->assertEquals($trade->status->value, TradeStatusEnum::PRE->value, '订单状态不一致');
 
+
+    // 再次创建一定报错
+    //$this->expectException(Throwable::class);
+    //$this->tradeCommandService->preCreate($command);
+
+
+    return $trade;
+
 });
+
+// 查询能获取支付方式
+
+test('can get trade pay methods', function (Trade $trade) {
+
+    $command         = new TradeReadyCommand();
+    $command->id     = $trade->id;
+    $command->scene  = SceneEnum::APP;
+    $command->device = 'mobile';
+    $command->client = 'alipay-ios-app';
+
+
+    $this->tradeCommandService->ready($command);
+
+
+})->depends('pre create a payment trade');
