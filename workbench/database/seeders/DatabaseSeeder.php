76af193dae2f59b1b64e1b6cd2dfda4a3794586d
liushoukun <liushoukun66@gmail.com>
2024-12-20 23:07:40 +0800

    build(testbench): 更新测试工作台配置和依赖
    - 修改 composer.json 文件，更新 PHP 版本要求和添加测试依赖
    - 创建测试工作台的路由、数据库迁移和种子文件
    - 更新 UserFactory 类，增加泛型类型注解- 调整 DatabaseSeeder 类，创建测试用户
    - 删除不必要的注释和空行
    - 添加 .gitignore 文件，忽略环境变量文件

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Workbench\Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Workbench\Database\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // UserFactory::new(10)->create();
+        // UserFactory::new()->times(10)->create();
 
-        // UserFactory::new()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        UserFactory::new()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
