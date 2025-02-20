a4e9f3a763a3b3963c53ea179bf87d1673f035fb
liushoukun <liushoukun66@gmail.com>
2025-02-20 23:37:40 +0800

    refactor(address): 优化地址表结构设计
    - 调整字段长度：owner_id、contacts、mobile、country、province、city、district、street
    - 修改 zip_code 字段注释为"邮编"
    - 更新 creator_type、updater_type 字段长度为 64
    - 为 order_addresses 表增加版本号字段

--- packages/address/database/migrations/2023_03_14_224125_create_address_table.php
+++ packages/address/database/migrations/2023_03_14_224125_create_address_table.php
@@ -9,17 +9,17 @@ return new class extends Migration {
     {
         Schema::create('address', function (Blueprint $table) {
             $table->id();
-            $table->string('owner_type')->comment('所属者类型');
-            $table->unsignedBigInteger('owner_id')->comment('所属者UID');
+            $table->string('owner_type',64)->comment('所属者类型');
+            $table->string('owner_id',64)->comment('所属者UID');
 
-            $table->string('contacts',300)->nullable()->comment('联系人');
-            $table->string('mobile')->nullable()->comment('手机');
+            $table->string('contacts',64)->nullable()->comment('联系人');
+            $table->string('mobile',64)->nullable()->comment('手机');
 
-            $table->string('country')->nullable()->comment('国家');
-            $table->string('province')->nullable()->comment('省份');
-            $table->string('city', 30)->nullable()->comment('城市');
-            $table->string('district', 40)->nullable()->comment('区县');
-            $table->string('street', 50)->nullable()->comment('乡镇街道');
+            $table->string('country',64)->nullable()->comment('国家');
+            $table->string('province',64)->nullable()->comment('省份');
+            $table->string('city', 64)->nullable()->comment('城市');
+            $table->string('district', 64)->nullable()->comment('区县');
+            $table->string('street', 64)->nullable()->comment('乡镇街道');
 
             $table->unsignedBigInteger('country_id')->nullable()->comment('国家ID');
             $table->unsignedBigInteger('province_id')->nullable()->comment('省份ID');
@@ -28,7 +28,7 @@ return new class extends Migration {
             $table->unsignedBigInteger('street_id')->nullable()->comment('乡镇街道ID');
 
             $table->string('address',500)->nullable()->comment('详细地址');
-            $table->string('zip_code', 10)->nullable()->comment('邮政编码');
+            $table->string('zip_code', 10)->nullable()->comment('邮编');
             $table->string('lon')->nullable()->comment('经度');
             $table->string('lat')->nullable()->comment('纬度');
             $table->string('tag')->nullable()->comment('标签');
@@ -38,11 +38,11 @@ return new class extends Migration {
             $table->integer('sort')->default(0)->comment('排序');
             $table->string('status',32)->nullable()->comment('状态');
 
-
-            $table->string('creator_type', 64)->nullable()->comment('创建者类型');
-            $table->unsignedBigInteger('creator_id')->nullable()->comment('创建者ID');
-            $table->string('updater_type', 64)->nullable()->comment('更新者类型');
-            $table->unsignedBigInteger('updater_id')->nullable()->comment('更新者UID');
+            $table->unsignedBigInteger('version')->default(0)->comment('版本');
+            $table->string('creator_type', 64)->nullable();
+            $table->string('creator_id', 64)->nullable();
+            $table->string('updater_type', 64)->nullable();
+            $table->string('updater_id', 64)->nullable();
 
             $table->timestamps();
             $table->softDeletes();
