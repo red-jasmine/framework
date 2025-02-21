ec2d1e613fcfcaca8bcea21c6e4ffacf86dbd982
liushoukun <liushoukun66@gmail.com>
2025-02-22 00:02:59 +0800

    refactor(order): 重构订单支付相关逻辑
    - 修改 OrderPayingCommand 结构，使用 Money 类型表示金额
    - 更新 OrderPayingCommandHandler 处理逻辑，支持全额支付
    - 新增 OrderPayCommand 和 OrderPayCommandHandler 用于购物模块
    - 重命名相关类和方法，提高代码可读性
    - 优化订单创建和支付流程，准备对接支付领域

--- packages/vip/src/Application/Services/VipPurchaseCommandService.php
+++ packages/vip/src/Application/Services/VipPurchaseCommandService.php
@@ -2,6 +2,8 @@
 
 namespace RedJasmine\Vip\Application\Services;
 
+use RedJasmine\Order\Application\Services\Orders\Commands\OrderPayingCommand;
+use RedJasmine\Order\Application\Services\Orders\OrderCommandService;
 use RedJasmine\Shopping\Application\Services\Commands\ProductBuyCommand;
 use RedJasmine\Shopping\Application\Services\ShoppingOrderCommandService;
 use RedJasmine\Support\Application\ApplicationCommandService;
@@ -11,7 +13,8 @@ class VipPurchaseCommandService extends ApplicationCommandService
 {
 
     public function __construct(
-        public ShoppingOrderCommandService $shoppingOrderCommandService
+        public ShoppingOrderCommandService $shoppingOrderCommandService,
+        public OrderCommandService $orderCommandService,
     ) {
     }
 
@@ -34,8 +37,18 @@ class VipPurchaseCommandService extends ApplicationCommandService
             ],
         ]);
 
-        $result = $this->shoppingOrderCommandService->buy($productBuyCommand);
-        dd($result);
+        $orders = $this->shoppingOrderCommandService->buy($productBuyCommand);
+
+        $orderPayingCommand = OrderPayingCommand::from([
+            'id' => $orders->first()->id,
+        ]);
+
+        // 订单发起支付
+        $orderPayingResult = $this->orderCommandService->paying($orderPayingCommand);
+
+        // 需要一个组合的组件 把 订单 和 支付组件联系在一起 TODO
+
+        dd($orderPayingResult);
     }
 
 
