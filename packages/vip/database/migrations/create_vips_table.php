1ee77fef89a7b4ab8059d366e5cbc5dde03e864d
liushoukun <liushoukun66@gmail.com>
2025-03-17 22:21:29 +0800

    refactor(vip): 移除 vips 表中的冗余字段
    - 删除了 create_vips_table.php 中的重复字段 'extras'- 优化了数据库表结构，减少了冗余信息

--- packages/vip/database/migrations/create_vips_table.php
+++ packages/vip/database/migrations/create_vips_table.php
@@ -17,9 +17,8 @@ return new class extends Migration {
             $table->string('icon')->nullable()->comment('图标');
             $table->string('description')->nullable()->comment('描述');
             $table->string('status')->default(VipStatusEnum::ENABLE)->comment(VipStatusEnum::comments('状态'));
-            $table->json('extras')->nullable()->comment('扩展信息');
-            $table->unsignedBigInteger('version')->default(0)->comment('版本');
             $table->json('extras')->nullable()->comment('扩展参数');
+            $table->unsignedBigInteger('version')->default(0)->comment('版本');
             $table->string('creator_type', 32)->nullable();
             $table->string('creator_id', 64)->nullable();
             $table->string('updater_type', 32)->nullable();
