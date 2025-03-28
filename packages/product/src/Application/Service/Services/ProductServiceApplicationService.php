0c9ad9020e65254a010578b62fa37715c7d376c8
liushoukun <liushoukun66@gmail.com>
2025-03-28 20:56:20 +0800

    refactor(product): 重构产品服务应用层
    - 移除了未使用的导入信息
    - 将 ApplicationCommandService 更改为 ApplicationService 作为基类
    - 调整了构造函数参数的可见性，使其成为公共属性
    - 删除了未使用的属性和方法

--- packages/product/src/Application/Service/Services/ProductServiceApplicationService.php
+++ packages/product/src/Application/Service/Services/ProductServiceApplicationService.php
@@ -2,16 +2,12 @@
 
 namespace RedJasmine\Product\Application\Service\Services;
 
-use Illuminate\Database\Eloquent\Model;
 use RedJasmine\Product\Domain\Service\Models\ProductService;
 use RedJasmine\Product\Domain\Service\Repositories\ProductServiceReadRepositoryInterface;
 use RedJasmine\Product\Domain\Service\Repositories\ProductServiceRepositoryInterface;
-use RedJasmine\Product\Domain\Tag\Models\ProductTag;
-use RedJasmine\Product\Domain\Tag\Repositories\ProductTagReadRepositoryInterface;
-use RedJasmine\Product\Domain\Tag\Repositories\ProductTagRepositoryInterface;
-use RedJasmine\Support\Application\ApplicationCommandService;
+use RedJasmine\Support\Application\ApplicationService;
 
-class ProductServiceApplicationService extends ApplicationCommandService
+class ProductServiceApplicationService extends ApplicationService
 {
     /**
      * 命令钩子前缀
@@ -23,10 +19,9 @@ class ProductServiceApplicationService extends ApplicationCommandService
 
 
     public function __construct(
-        protected ProductServiceRepositoryInterface     $repository,
-        protected ProductServiceReadRepositoryInterface $readRepository
-    )
-    {
+        public ProductServiceRepositoryInterface $repository,
+        public ProductServiceReadRepositoryInterface $readRepository
+    ) {
     }
 
 }
