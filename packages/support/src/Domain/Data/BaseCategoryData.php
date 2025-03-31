81835952f35868f63c7de94c6847e4f540339677
liushoukun <liushoukun66@gmail.com>
2025-04-01 00:08:24 +0800

    refactor(article): 重构文章分类相关代码
    - 将 ArticleCategoryData 和 ArticleCategory 模型中的通用分类功能提取到 BaseCategoryData 和 BaseCategoryModel 中
    - 删除了 ArticleCategory 模型中的冗余代码和未使用的 CategoryStatusEnum
    - 优化了代码结构，提高了代码的可维护性和可复用性

---
+++ packages/support/src/Domain/Data/BaseCategoryData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RedJasmine\Support\Domain\Data;
+
+use RedJasmine\Support\Data\Data;
+use RedJasmine\Support\Domain\Models\Enums\CategoryStatusEnum;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\EnumCast;
+use Spatie\LaravelData\Support\Validation\ValidationContext;
+
+class BaseCategoryData extends Data
+{
+    public string             $name;
+    public ?string            $description = null;
+    public int                $parentId    = 0;
+    #[WithCast(EnumCast::class, CategoryStatusEnum::class)]
+    public CategoryStatusEnum $status      = CategoryStatusEnum::ENABLE;
+    public int                $sort        = 0;
+    public bool               $isLeaf      = false;
+    public bool               $isShow      = false;
+    public ?string            $image       = null;
+    public ?string            $cluster     = null;
+
+
+    public static function rules(ValidationContext $context) : array
+    {
+
+        return [
+            'id'          => [],
+            'parent_id'   => ['integer'],
+            'name'        => ['required', 'string', 'max:100'],
+            'description' => ['sometimes', 'nullable', 'string', 'max:255'],
+            'description' => ['cluster', 'nullable', 'string', 'max:255'],
+            'image'       => ['sometimes', 'nullable', 'max:255'],
+            'extras'      => ['sometimes', 'nullable', 'array'],
+        ];
+
+    }
+}
