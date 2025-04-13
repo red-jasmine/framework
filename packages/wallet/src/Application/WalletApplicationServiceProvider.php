892911dbfc2f7a00cd1a17f7902fa111416043be
liushoukun <liushoukun66@gmail.com>
2025-04-13 16:29:39 +0800

    feat(wallet): 添加钱包交易相关接口和实现类
    - 新增 WalletTransactionRepositoryInterface 和 WalletTransactionReadRepositoryInterface 接口
    - 新增 WalletTransactionRepository 和 WalletTransactionReadRepository 实现类
    - 在 WalletApplicationServiceProvider 中绑定新接口和实现类

--- packages/wallet/src/Application/WalletApplicationServiceProvider.php
+++ packages/wallet/src/Application/WalletApplicationServiceProvider.php
@@ -6,9 +6,13 @@ use Illuminate\Support\ServiceProvider;
 use RedJasmine\Wallet\Application\Listeners\WalletWithdrawalApprovalListener;
 use RedJasmine\Wallet\Domain\Repositories\WalletReadRepositoryInterface;
 use RedJasmine\Wallet\Domain\Repositories\WalletRepositoryInterface;
+use RedJasmine\Wallet\Domain\Repositories\WalletTransactionReadRepositoryInterface;
+use RedJasmine\Wallet\Domain\Repositories\WalletTransactionRepositoryInterface;
 use RedJasmine\Wallet\Domain\Repositories\WalletWithdrawalRepositoryInterface;
 use RedJasmine\Wallet\Infrastructure\ReadRepositories\Mysql\WalletReadRepository;
+use RedJasmine\Wallet\Infrastructure\ReadRepositories\Mysql\WalletTransactionReadRepository;
 use RedJasmine\Wallet\Infrastructure\Repositories\Eloquent\WalletRepository;
+use RedJasmine\Wallet\Infrastructure\Repositories\Eloquent\WalletTransactionRepository;
 use RedJasmine\Wallet\Infrastructure\Repositories\Eloquent\WalletWithdrawalRepository;
 
 class WalletApplicationServiceProvider extends ServiceProvider
@@ -21,6 +25,11 @@ class WalletApplicationServiceProvider extends ServiceProvider
 
         $this->app->bind(WalletWithdrawalRepositoryInterface::class, WalletWithdrawalRepository::class);
 
+
+
+
+        $this->app->bind(WalletTransactionRepositoryInterface::class, WalletTransactionRepository::class);
+        $this->app->bind(WalletTransactionReadRepositoryInterface::class, WalletTransactionReadRepository::class);
 
     }
 
