517a8f50d9c9bd8f50f5034a91bf4b71b5885f04
liushoukun <liushoukun66@gmail.com>
2025-04-20 22:50:12 +0800

    refactor: address
    - 修改地址

--- packages/address/src/AddressServiceProvider.php
+++ packages/address/src/AddressServiceProvider.php
@@ -2,81 +2,26 @@
 
 namespace RedJasmine\Address;
 
-use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class AddressServiceProvider extends ServiceProvider
+class AddressServiceProvider extends PackageServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot(): void
+    public static string $name = 'red-jasmine-address';
+
+    public static string $viewNamespace = 'red-jasmine-address';
+
+
+    public function configurePackage(Package $package) : void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'red-jasmine');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'red-jasmine');
-         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-    }
+        $package->name(static::$name)
+                ->hasConfigFile()
+                ->hasMigrations([
+                    'create_address_table',
+                ])
+                ->runsMigrations();
 
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/address.php', 'address');
 
-        // Register the service the package provides.
-        $this->app->singleton('address', function ($app) {
-            return new Address;
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['address'];
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole(): void
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/address.php' => config_path('address.php'),
-        ], 'address.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/red-jasmine'),
-        ], 'address.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/red-jasmine'),
-        ], 'address.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/red-jasmine'),
-        ], 'address.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
