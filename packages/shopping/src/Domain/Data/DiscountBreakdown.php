c4dc9e231e44d7533b5136eb04332c55ac9c1ea7
liushoukun <liushoukun66@gmail.com>
2025-07-03 00:03:34 +0800

    refactor(shopping): 重构优惠明细数据类
    - 创建新的 DiscountBreakdown 类，位于 shopping/src/Domain/Data/ 目录下- 使用 Cknow\Money\Money 类替换原有的 RedJasmine\Support\Domain\Models\ValueObjects\MoneyOld 类
    - 删除旧的 DiscountBreakdown 类，位于 shopping/src/Domain/Orders/Data/ 目录下

---
+++ packages/shopping/src/Domain/Data/DiscountBreakdown.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RedJasmine\Shopping\Domain\Data;
+
+use Cknow\Money\Money;
+use RedJasmine\Support\Data\Data;
+
+/**
+ * 优惠明细
+ */
+class DiscountBreakdown extends Data
+{
+
+    public Money $total;
+
+}
