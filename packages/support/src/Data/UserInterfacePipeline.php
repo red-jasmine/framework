d25f2b011be01671ebec35eb830e4ef20866369b
liushoukun <liushoukun66@gmail.com>
2025-07-16 10:04:11 +0800

    refactor(UserInterfacePipeline): 优化用户接口管道处理逻辑
    - 使用 Str::snake() 方法将属性名转换为蛇形命名，提高代码兼容性
    - 修复使用下划线连接属性名和关键字的逻辑，确保正确处理复合键名
    - 优化条件判断，提高代码可读性和维护性

--- packages/support/src/Data/UserInterfacePipeline.php
+++ packages/support/src/Data/UserInterfacePipeline.php
@@ -2,6 +2,7 @@
 
 namespace RedJasmine\Support\Data;
 
+use Illuminate\Support\Str;
 use RedJasmine\Support\Contracts\UserInterface;
 use Spatie\LaravelData\DataPipes\DataPipe;
 use Spatie\LaravelData\Support\Creation\CreationContext;
@@ -16,19 +17,19 @@ class UserInterfacePipeline implements DataPipe
 
             if ($property->type->type->acceptsType(UserInterface::class) || $property->type->type->findAcceptedTypeForBaseType(UserInterface::class)) {
 
-                $idKey   = $property->name . '_id';
-                $typeKey = $property->name . '_type';
-
+                $name    = Str::snake($property->name);
+                $idKey   = $name.'_id';
+                $typeKey = $name.'_type';
                 if (isset($payload[$idKey], $payload[$typeKey])
-                    && !isset($payload[$property->name])
+                    && !isset($payload[$name])
 
                     && filled($payload[$idKey]) && filled($payload[$typeKey])
                 ) {
                     $properties[$property->name] = UserData::from([
                         'id'       => $payload[$idKey],
                         'type'     => $payload[$typeKey],
-                        'avatar'   => $payload[$property->name . '_avatar'] ?? null,
-                        'nickname' => $payload[$property->name . '_nickname'] ?? null,
+                        'avatar'   => $payload[$property->name.'_avatar'] ?? null,
+                        'nickname' => $payload[$property->name.'_nickname'] ?? null,
                     ]);
                 }
 
