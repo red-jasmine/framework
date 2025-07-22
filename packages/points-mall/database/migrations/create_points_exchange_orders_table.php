4e200c7069c88e64b1895cee7d2ba9c605a691e5
liushoukun <liushoukun66@gmail.com>
2025-07-22 18:30:32 +0800

    feat(points-mall): 优化积分兑换订单表结构
    - 添加 userMorphs 方法实现多态关联，支持不同类型的用户
    -调整 owner_id 和 owner_type 的索引顺序，提高查询效率
    - 添加 user_id 和 user_id 的索引，优化用户相关查询

--- packages/points-mall/database/migrations/create_points_exchange_orders_table.php
+++ packages/points-mall/database/migrations/create_points_exchange_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
 
             $table->string('owner_type', 32)->comment('所属者类型');
             $table->string('owner_id', 64)->comment('所属者ID');
+            $table->userMorphs('user', '用户', false);
             $table->string('order_no', 64)->unique()->comment('兑换订单号');
             $table->string('outer_order_no')->comment('关联订单号');
             $table->unsignedBigInteger('point_product_id')->comment('积分商品ID');
@@ -29,16 +30,17 @@ return new class extends Migration {
             $table->string('status')->default(PointsExchangeOrderStatusEnum::EXCHANGED->value)->comment('状态');
             $table->timestamp('exchange_time')->comment('兑换时间');
 
-            
+
             $table->operator();
             $table->softDeletes();
             $table->comment('积分兑换订单表');
-            
+
             // 索引
             $table->index('order_id', 'idx_order_id');
             $table->index('product_id', 'idx_product_id');
             $table->index('status', 'idx_status');
-            $table->index(['owner_type', 'owner_id'], 'idx_owner');
+            $table->index(['owner_id', 'owner_type'], 'idx_owner');
+            $table->index(['user_id', 'user_id'], 'idx_user');
         });
     }
 
