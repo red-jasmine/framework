674d125262c2d55af7fe966f30c60932f000fd04
liushoukun <liushoukun66@gmail.com>
2025-08-18 23:33:28 +0800

    refactor(message): 重构消息模块路由并更新依赖版本
    -移除了 admin、shop 和 web路由的定义
    - 更新了 composer.json 文件，添加 branch-alias 配置
    - 在根目录 composer.json 中添加 red-jasmine/message 依赖

--- packages/message/routes/message.php
+++ packages/message/routes/message.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use RedJasmine\Message\UI\Http\User\MessageUserRoute;
-use RedJasmine\Message\UI\Http\Admin\MessageAdminRoute;
-use RedJasmine\Message\UI\Http\Shop\MessageShopRoute;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +18,3 @@ Route::group(['prefix' => 'api/user', 'middleware' => ['api']], function () {
     MessageUserRoute::api();
 });
 
-// 管理端路由
-Route::group(['prefix' => 'api/admin', 'middleware' => ['api']], function () {
-    MessageAdminRoute::api();
-});
-
-// 商家端路由
-Route::group(['prefix' => 'api/shop', 'middleware' => ['api']], function () {
-    MessageShopRoute::api();
-});
-
-// Web路由
-Route::group(['middleware' => ['web']], function () {
-    MessageUserRoute::web();
-    MessageAdminRoute::web();
-    MessageShopRoute::web();
-});
